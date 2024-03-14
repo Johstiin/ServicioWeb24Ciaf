ed8c1dfff82e99271a136c38115ac906ecf00cd7
Johstin <johsting0021@gmail.com>
2024-03-13 20:33:03 -0500

    ecuaciones js

---
+++ operadores.js
@@ -0,0 +1,19 @@
+let counter = 100;
+//suma
+counter = counter + 20;
+counter += 30;
+//Resta
+counter = counter - 20;
+counter -= 30;
+//multiplicacion
+counter = counter*3;
+counter *= 4;
+//division
+counter = counter/5;
+counter /= 5;
+//Residuo
+counter = counter%3;
+counter %= 3;
+//exponencial
+counter = counter**3;
+counter **=2;
