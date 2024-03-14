fb618ff6014274f47c23fe0559a3e7317be0fc3d
Johstin <johsting0021@gmail.com>
2024-03-13 20:51:10 -0500

    ultimo cambio de ecuaciones

--- operadores.js
+++ operadores.js
@@ -1,21 +1,26 @@
+console.log("operadores...")
 let counter = 100;
 //suma
 counter = counter + 20;
 counter += 30;
+console.log(counter)
 //Resta
 counter = counter - 20;
 counter -= 30;
+console.log(counter)
 //multiplicacion
 counter = counter*3;
 counter *= 4;
+console.log(counter)
 //division
 counter = counter/5;
 counter /= 5;
+console.log(counter)
 //Residuo
 counter = counter%3;
 counter %= 3;
+console.log(counter)
 //exponencial
 counter = counter**3;
 counter **=2;
-
 console.log(counter)
